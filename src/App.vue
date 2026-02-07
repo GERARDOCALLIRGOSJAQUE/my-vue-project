36bd180544412c49d939c733172974a0eef63a97
Gerardo Callirgos Jaque <gcallirgosjaque@gmail.com>
2026-02-07 17:46:01 -0500

    Decimo commit: v-for index y v-for object eb Vue

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,40 @@
 <script setup>
 import HeaderComponent from './components/HeaderComponent.vue';
 import { ref } from 'vue';
-  //let number = 0;//variable no reactiva
-  let number = ref(0);//variable reactiva
-  /*setInterval(() => {
-    number.value++;
-  }, 1000);
-  const addOne = () => {
-    number.value++;
-  };
-  
-  const myValue = ref('patata');
-  let myCondition = true;
-  const condition = '';
-  const condition1 = false;*/
   const movies = ["Lord of the Rings", "Harry Potter", "Star Wars"];
+const animals = [
+    {
+        id: 1,
+        name: 'León',
+        img: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 2,
+        name: 'Tigre',
+        img: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 3,
+        name: 'Elefante',
+        img: 'https://via.placeholder.com/150'
+    }
+]
+
 </script>
 
 <template>
-  <!--<HeaderComponent></HeaderComponent>
-  <h1>{{ myValue}}</h1> v-bind:id="myId" = :id="myId"-->
-  <!--<h1>Directiva v-if</h1>
-  <h2 v-if="condition">Condicion 1 if</h2>
-  <h2 v-else-if="condition1">Condicion 2 else-if</h2>
-  <h2 v-else>Condicion 3 else</h2>
-  <input type="text" v-model="myValue"> v-bindcrear reactividad"
-  <h2 @click="addOne">Contador {{ number }}</h2>
-  <h1 :class="myCondition ? 'title' : 'black'">Clase dinamica</h1>
-  <h1 :class="{title: myCondition, black: true}">Clase dinamica</h1>-->
-  <h1>Rederizado de lista | v-for</h1>
+  <h1>Renderizado de lista | v-for</h1>
   <div>
-    <p v-for="movie in movies" :key="movie">
-      {{ movie }}
+    V-for index
+    <p v-for="(movie, index) in movies" :key="movie">
+      {{ index + 1 }} - {{ movie }}
     </p>
+    <ul>
+      <li v-for="animal in animals" :key="animal.id">
+        {{ animal.name }}
+        <img :src="animal.img" width="200px"/>
+      </li>
+    </ul>
   </div>
 </template>
 
---
+++ src/data/animals.js
@@ -0,0 +1,17 @@
+const animals = [
+    {
+        id: 1,
+        name: 'León',
+        img: ''
+    },
+    {
+        id: 2,
+        name: 'Tigre',
+        img: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 3,
+        name: 'Elefante',
+        img: 'https://via.placeholder.com/150'
+    }
+]
