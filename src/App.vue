23f7d358288e5e6b7941cbe7257e0254e7ca9b4b
Gerardo Callirgos Jaque <gcallirgosjaque@gmail.com>
2026-02-07 11:58:58 -0500

    Cuarto commit: Eventos click con Vue

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@ import HeaderComponent from './components/HeaderComponent.vue';
 import { ref } from 'vue';
   //let number = 0;//variable no reactiva
   let number = ref(0);//variable reactiva
-
-
-  setInterval(() => {
+  /*setInterval(() => {
     number.value++;
-  }, 1000);
+  }, 1000);*/
+  const addOne = () => {
+    number.value++;
+  };
+  
 </script>
 
 <template>
   <HeaderComponent></HeaderComponent>
   <h1 class="title">Hola a todos!</h1>
-  <h1>Contador {{ number }}</h1>
+  <h2 @click="addOne">Contador {{ number }}</h2>
 </template>
 
 <style scoped>
