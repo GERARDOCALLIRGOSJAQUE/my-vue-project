9a7c43bf9b8de35bd35c972577c335db7d903ae2
Gerardo Callirgos Jaque <gcallirgosjaque@gmail.com>
2026-02-07 11:40:35 -0500

    Tercer commit: variable no reactivas y reactivas en Vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
 import HeaderComponent from './components/HeaderComponent.vue';
+import { ref } from 'vue';
+  //let number = 0;//variable no reactiva
+  let number = ref(0);//variable reactiva
 
 
+  setInterval(() => {
+    number.value++;
+  }, 1000);
 </script>
 
 <template>
-  <HeaderComponent>
-  </HeaderComponent>
+  <HeaderComponent></HeaderComponent>
   <h1 class="title">Hola a todos!</h1>
-
+  <h1>Contador {{ number }}</h1>
 </template>
 
 <style scoped>
