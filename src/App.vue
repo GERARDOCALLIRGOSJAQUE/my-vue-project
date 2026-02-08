0b321b8aab44b23ed2021991bb18dfaeb4d3b56d
Gerardo Callirgos Jaque <gcallirgosjaque@gmail.com>
2026-02-07 21:15:16 -0500

    Onceavo commit: componente padre e hijo Vue

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,19 @@
 <script setup>
 import HeaderComponent from './components/HeaderComponent.vue';
 import { ref } from 'vue';
-  const movies = ["Lord of the Rings", "Harry Potter", "Star Wars"];
-const animals = [
-    {
-        id: 1,
-        name: 'León',
-        img: 'https://via.placeholder.com/150'
-    },
-    {
-        id: 2,
-        name: 'Tigre',
-        img: 'https://via.placeholder.com/150'
-    },
-    {
-        id: 3,
-        name: 'Elefante',
-        img: 'https://via.placeholder.com/150'
-    }
-]
+import ChildComponent from './components/ChildComponent.vue';
 
+  const text = "Info desde variable padre";
+  const person = {
+    sex: "hombre",
+    age: 30
+  }
 </script>
 
 <template>
   <h1>Renderizado de lista | v-for</h1>
-  <div>
-    V-for index
-    <p v-for="(movie, index) in movies" :key="movie">
-      {{ index + 1 }} - {{ movie }}
-    </p>
-    <ul>
-      <li v-for="animal in animals" :key="animal.id">
-        {{ animal.name }}
-        <img :src="animal.img" width="200px"/>
-      </li>
-    </ul>
-  </div>
+  <ChildComponent :msg="text" :person="person"></ChildComponent>
+  <ChildComponent msg="Otro mensaje" ></ChildComponent>
 </template>
 
 <style scoped>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    msg: String,
+    person: {
+        sex: String,
+        age: Number
+    }
+})
+</script>
+<template>
+    <h2>Este es un componente hijo</h2>
+    <p>{{msg}}</p>
+    <div> {{person?.sex}} - {{person?.age}} </div>
+</template>
+<style>
+</style>
