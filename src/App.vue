7c0854b0f469de048dbacfb3d1677c1a34806226
Gerardo Callirgos Jaque <gcallirgosjaque@gmail.com>
2026-02-11 17:00:01 -0500

    16th commit: formulario usando vue.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <script setup>
-import { ref, watch, watchEffect } from "vue";
+import { reactive, ref, watch, watchEffect } from "vue";
 
-//const userName = ref({name: "Adrián"});
-const userName = ref("Adrián");
-const setName = () => { 
-  userName.value = "Maria";
-  };
-
-  /*watch(userName, 
-  (newValue, oldValue) => {
-    console.log("User name modified");
-  }, {
-    immediate: true,
-    deep: true
-  })*/
- watchEffect(() => {
-    console.log("User name modified to: ", userName.value);
-  })
+const movie = reactive({
+  title: "",
+  duration: 0,
+  director: ""
+})
+const handleSubmit = () => {
+  if(!movie.title || !movie.duration || !movie.director) {
+    alert("Please fill in all fields");
+    return;
+  }
+  console.log(movie);
+}
 </script>
 
 <template>
-  <h1>Watch | Vigilancia de cambios</h1>
-  <h2>Hola {{ userName }}</h2>
-  <input type="text" v-model="userName">
-  <button @click="setName">Cambiar nombre</button>
+  <h1>Forms</h1>
+  <form @submit.prevent="handleSubmit" class="form">
+    <label for="title">Title</label>
+    <input type="text" id="title" v-model="movie.title">
+
+    <label for="duration">Duration</label>
+    <input type="number" id="duration" v-model = "movie.duration">
+
+    <label for="director">Director</label>
+    <input type="text" id="director" v-model = "movie.director">
+    <button>Submit</button>
+  </form>
 </template>
 
 <style scoped>
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+  }
   .title {
     background-color: blue;
     color: red;
